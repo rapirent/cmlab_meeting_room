ebf6e8b8393f73c152ee414736db42b6e542c880
rapirent <rapirent@gmail.com>
2019-07-21 02:52:43 +0800

    i hate csrf

--- reserve_meeting_room.py
+++ reserve_meeting_room.py
@@ -1,6 +1,8 @@
 # encoding=utf8
+from bs4 import BeautifulSoup
 import argparse
 import requests
+import re
 import sys
 
 parser = argparse.ArgumentParser()
@@ -22,7 +24,8 @@ if __name__ == '__main__':
         'password': args.password
     }
     try:
-        s.post('https://esystem.csie.ntu.edu.tw/login', data=form_data)
+        r = s.post('https://esystem.csie.ntu.edu.tw/login', data=form_data)
+        assert r.status_code == requests.codes.ok
     except requests.exceptions.HTTPError as e:
         print(e)
         sys.exit(1)
@@ -38,7 +41,19 @@ if __name__ == '__main__':
         'description': args.description
     }
     try:
-        s.post('https://esystem.csie.ntu.edu.tw/room', data=form_data)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        csrftoken = soup.find('meta', attrs={'name': 'csrf-token'})['content']
+        assert csrftoken != ''
+        header = {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRF-Token': csrftoken,
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+        }
+        a = s.post('https://esystem.csie.ntu.edu.tw/room/register', data=form_data, headers=header)
+        assert a.status_code == requests.codes.ok
+        print(a.text)
     except requests.exceptions.HTTPError as e:
         print(e)
         sys.exit(1)
